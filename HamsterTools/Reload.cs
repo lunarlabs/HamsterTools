79c8d6aa2954413c7e63ae04003500b698871bf3
ichiro <aussieevil@hotmail.com>
2015-01-14 22:07:32 -0800

    Initial commit

---
+++ HamsterTools/Reload.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamsterTools.Reload
+{
+    class Reload
+    {
+
+    }
+
+    class ReloadNode
+    {
+        //Declarations
+        private ReloadNodeType type;
+        private string nodeName;
+        private byte byteData;
+        private short int16Data;
+        private int int32Data;
+    }
+
+    enum ReloadNodeType
+    {
+        Null, Byte, Int16, Int32, Int64, Double, String
+    }
+}
