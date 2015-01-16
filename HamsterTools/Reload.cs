4f69631c3daa37121711c9f674fe2f7497666d37
ichiro <aussieevil@hotmail.com>
2015-01-15 20:11:58 -0800

    Added constructors to ReloadNode

--- HamsterTools/Reload.cs
+++ HamsterTools/Reload.cs
@@ -20,6 +20,53 @@ namespace HamsterTools.Reload
         private byte byteData;
         private short int16Data;
         private int int32Data;
+        private long int64Data;
+        private double doubleData;
+        private string stringData;
+        private List<ReloadNode> children = new List<ReloadNode>();
+
+        //constructors
+        public ReloadNode()
+        {
+            //No argument means a null node type.
+            type = ReloadNodeType.Null;
+        }
+
+        public ReloadNode(byte bData){
+            type=ReloadNodeType.Byte;
+            byteData = bData;
+        }
+
+        public ReloadNode(short sData)
+        {
+            type = ReloadNodeType.Int16;
+            int16Data = sData;
+        }
+
+        public ReloadNode(int iData)
+        {
+            type = ReloadNodeType.Int32;
+            int32Data = iData;
+        }
+
+        public ReloadNode(long lData)
+        {
+            type = ReloadNodeType.Int64;
+            int64Data = lData;
+        }
+
+        public ReloadNode(double dData)
+        {
+            type = ReloadNodeType.Double;
+            doubleData = dData;
+        }
+
+        public ReloadNode(string sData)
+        {
+            type = ReloadNodeType.String;
+            stringData = sData;
+        }
+
     }
 
     enum ReloadNodeType
