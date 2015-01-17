4ec6611e90afdab5c523a86edde61eb4563e9193
ichiro <aussieevil@hotmail.com>
2015-01-16 19:15:24 -0800

    Added getters and setter to ReloadNode

---
+++ HamsterTools/OHRRPGCE.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamsterTools
+{
+    public static class OHRRPGCE
+    {
+        //This is the big constants file. See const.bi in OHRRPGCE source.
+        /*
+         *OHRRPGCE CUSTOM constants 
+         */
+        const int CurrentRPGVersion = 19;
+        // It is a good idea to increment this number each time a major feature
+        // has been added, if opening a new game in an old editor would cause data-loss
+        // Don't be afraid to increment this. Backcompat warnings are a good thing!
+        //--version history
+        // 7 - ypsiliform wip added > 36 NPC defs (and many other features)
+        // 8 - ypsiliform wip added extended chaining data (and many other features)
+        // 9 - ypsiliform wip added text box sound effects
+        // 10 - ypsiliform wip added attack-based enemy transmogrification
+        // 11 - zenzizenzic wip added variable record size and record number .N## lumps
+        // 12 - zenzizenzic wip increased .N## record size
+        // 13 - zenzizenzic wip changed password format to PW4, older versions have broken genPassVersion handling
+        // 14 - zenzizenzic wip made .DT0 binsize-sized
+        // 15 - zenzizenzic wip made .DT1 binsize-sized, and added binsize.bin, fixbits.bit safeguards
+        // 16 - zenzizenzic wip made .ITM binsize-sized
+        // 17 - alectormancy wip increase global limit from 4095 to 16383
+        // 18 - beelzebufo turn-based support
+        // 19 - beelzebufo replaced .DT0 with heroes.reld
+        const int CurrentRSAVVersion = 3;
+        // Increment this number any time that loading and resaving a game in either
+        // new or old versions of Game leads to data-loss, or major new features are
+        // added.
+        // 1 - zenzizenzic wip removed nativehbits related nodes
+        // 2 - alectormancy wip increased global limit from 4095 to 16383
+        // 3 - stopped writing obsolete battle_menus node
+
+        
+        public enum Stats
+        {
+            HP, MP, Attack, Aim, Defense, Dodge, Magic, Will, Speed, Counter, Focus, ExtraHits, User
+        }
+        const int LastStat = 11;
+
+        public enum StatsAt
+        {
+            Lv0, Lv99
+        }
+
+        public enum Fix //quirks bits
+        {
+            AttackItems, WeaponPoints, StunCancelTarget, DefaultDissolve, DefaultEnemyDissolve, PushNPCBugCompatibility,
+            DefaultMaxItem, BlankDoorLinks, ShopSounds, ExtendedNPCs, HeroPortrait, TextBoxPortrait, NPCLocationFormat,
+            InitDamageDisplay, DefaultLevelCap, HeroElementals, OldElementalFailBit, AttackElementFails, EnemyElementals,
+            ItemElementals, NumElements, RemoveDamageMP, DefaultMaxLevel, Unused23
+        }
+        const int FixBitsSize = 23;
+
+
+        /*
+         * RELOAD constants
+         */
+
+        const byte[] ReloadMagicNumber = { 0x52, 0x45, 0x4c, 0x44 }; 
+        // "RELD" ... if it doesn't have this as the first 4 bytes it's NOT a reload file!
+        const byte ReloadVersionNumber = 1;
+        // Remember to keep up to date with the email list about RELOAD developments.
+        const long ReloadHeaderSize = 13;
+        // Why a long? Ask TMC or whoever made the RELOAD spec...
+    }
+}
--- HamsterTools/Reload.cs
+++ HamsterTools/Reload.cs
@@ -7,22 +7,206 @@ using System.Threading.Tasks;
 
 namespace HamsterTools.Reload
 {
-    class Reload
-    {
-
-    }
 
     class ReloadNode
     {
         //Declarations
         private ReloadNodeType type;
+        public ReloadNodeType Type
+        {
+            get
+            {
+                return type;
+            }
+        }
+
         private string nodeName;
+        public string Name
+        {
+            get
+            {
+                return nodeName;
+            }
+            set
+            {
+                nodeName = value;
+            }
+        }
+
         private byte byteData;
+        public byte ByteData
+        {
+            get
+            {
+                if (type == ReloadNodeType.Byte)
+                {
+                    return byteData;
+                }
+                else
+                {
+                    string err = "Wrong node type: Tried to read Byte value from a " + type.ToString() + " node.";
+                    throw new InvalidOperationException(err);
+                }
+            }
+            set
+            {
+                if (type == ReloadNodeType.Byte)
+                {
+                    byteData = value;
+                }
+                else
+                {
+                    string err = "Wrong node type: tried to pass a Byte value to a " + type.ToString() + " node.";
+                    throw new InvalidOperationException(err);
+                }
+            }
+        }
+
         private short int16Data;
+        public short Int16Data
+        {
+            get
+            {
+                if (type == ReloadNodeType.Int16)
+                {
+                    return int16Data;
+                }
+                else
+                {
+                    string err = "Wrong node type: Tried to read Int16 value from a " + type.ToString() + " node.";
+                    throw new InvalidOperationException(err);
+                }
+            }
+            set
+            {
+                if (type == ReloadNodeType.Int16)
+                {
+                    int16Data = value;
+                }
+                else
+                {
+                    string err = "Wrong node type: tried to pass a Int16 value to a " + type.ToString() + " node.";
+                    throw new InvalidOperationException(err);
+                }
+            }
+        }
+
         private int int32Data;
+        public int Int32Data
+        {
+            get
+            {
+                if (type == ReloadNodeType.Int32)
+                {
+                    return int32Data;
+                }
+                else
+                {
+                    string err = "Wrong node type: Tried to read Int32 value from a " + type.ToString() + " node.";
+                    throw new InvalidOperationException(err);
+                }
+            }
+            set
+            {
+                if (type == ReloadNodeType.Int32)
+                {
+                    int32Data = value;
+                }
+                else
+                {
+                    string err = "Wrong node type: tried to pass a Int32 value to a " + type.ToString() + " node.";
+                    throw new InvalidOperationException(err);
+                }
+            }
+        }
+
         private long int64Data;
+        public long Int64Data
+        {
+            get
+            {
+                if (type == ReloadNodeType.Int64)
+                {
+                    return int64Data;
+                }
+                else
+                {
+                    string err = "Wrong node type: Tried to read Int64 value from a " + type.ToString() + " node.";
+                    throw new InvalidOperationException(err);
+                }
+            }
+            set
+            {
+                if (type == ReloadNodeType.Int64)
+                {
+                    int64Data = value;
+                }
+                else
+                {
+                    string err = "Wrong node type: tried to pass a Int64 value to a " + type.ToString() + " node.";
+                    throw new InvalidOperationException(err);
+                }
+            }
+        }
+
         private double doubleData;
+        public double DoubleData
+        {
+            get
+            {
+                if (type == ReloadNodeType.Double)
+                {
+                    return doubleData;
+                }
+                else
+                {
+                    string err = "Wrong node type: Tried to read Double value from a " + type.ToString() + " node.";
+                    throw new InvalidOperationException(err);
+                }
+            }
+            set
+            {
+                if (type == ReloadNodeType.Double)
+                {
+                    doubleData = value;
+                }
+                else
+                {
+                    string err = "Wrong node type: tried to pass a Double value to a " + type.ToString() + " node.";
+                    throw new InvalidOperationException(err);
+                }
+            }
+        }
+        
         private string stringData;
+        public String StringData
+        {
+            get
+            {
+                if (type == ReloadNodeType.String)
+                {
+                    return stringData;
+                }
+                else
+                {
+                    string err = "Wrong node type: Tried to read String value from a " + type.ToString() + " node.";
+                    throw new InvalidOperationException(err);
+                }
+            }
+            set
+            {
+                if (type == ReloadNodeType.String)
+                {
+                    stringData = value;
+                }
+                else
+                {
+                    string err = "Wrong node type: tried to pass a String value to a " + type.ToString() + " node.";
+                    throw new InvalidOperationException(err);
+                }
+            }
+        }
+
         private List<ReloadNode> children = new List<ReloadNode>();
 
         //constructors
@@ -66,6 +250,8 @@ namespace HamsterTools.Reload
             type = ReloadNodeType.String;
             stringData = sData;
         }
+
+        //TODO Add methods related to children.
 
     }
 
