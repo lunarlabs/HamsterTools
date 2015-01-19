ba3296b2c60b39ec206744ee5439dba5d4701aba
ichiro <aussieevil@hotmail.com>
2015-01-19 12:32:03 -0800

    LumpManager changes

--- HamsterTools/LumpManager.cs
+++ HamsterTools/LumpManager.cs
@@ -12,18 +12,24 @@ namespace HamsterTools
         // private const long[] PDPOffsets = { 256 ^ 2, 256 ^ 3, 256 ^ 0, 256 ^ 1 };
         // Only used here for some reason. Used to get the length of lumps.
 
-        //high, highest, lowest, low
+        // PDP endianness: high, highest, lowest, low
 
         private System.IO.File lump;
         private System.IO.Directory unlumpedDirectory;
+        private byte[] _length;
 
+        
         public static void unLump(string sourceFileLocation, string destinationFileName)
         {
+            Console.WriteLine("Unlumping " + Path.GetFileName(sourceFileLocation) + " to " 
+                + Path.GetDirectoryName(destinationFileName) +"...");
 
         }
 
         public static void unLump(string sourceFileLocation)
         {
+            Console.WriteLine("No destination directory specified, unlumping to " + Path.GetFileName(sourceFileLocation) 
+                + ".hwtmp...");
             string tempDir = Path.GetFullPath(sourceFileLocation) + ".hwtwmp";
             unLump(sourceFileLocation, tempDir);
         }
