03da4bedef1c12a29a301ecc5af81f4a9e9ac8f7
ichiro <aussieevil@hotmail.com>
2015-01-26 17:20:13 -0800

    Added RPG class

---
+++ HamsterTools/RPG.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamsterTools
+{
+    public sealed class RPG
+    {
+        private static readonly RPG instance = new RPG();
+
+        static RPG()
+        {
+
+        }
+
+        private RPG()
+        {
+
+        }
+
+        public static RPG Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void ReadFromFile(string fileName)
+        {
+
+        }
+
+        public void WriteToFile(string fileName)
+        {
+
+        }
+    }
+}
