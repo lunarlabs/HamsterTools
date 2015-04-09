a6d77158feb293e20962e8bb51b8dc14a5924621
ichiro <aussieevil@hotmail.com>
2015-04-09 03:08:13 -0700

    RPG file

--- HamsterTools/RPG.cs
+++ HamsterTools/RPG.cs
@@ -4,18 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HamsterTools
+namespace HamsterTools.Properties
 {
     public sealed class RPG
     {
-        private static readonly RPG instance = new RPG();
-
-        private string archnym;
-
-
-        static RPG()
+        //Singleton methods
+        private static RPG _instance = null;
+        public static RPG Instance
         {
-
+            get
+            {
+                return _instance;
+            }
+        }
+        private LoadedState _state = LoadedState.NotLoaded;
+        public LoadedState State
+        {
+            get
+            {
+                return _state;
+            }
         }
 
         private RPG()
@@ -23,22 +31,60 @@ namespace HamsterTools
 
         }
 
-        public static RPG Instance
+        #region Lump Data
+
+        private string _fileLocation;
+        private string _folder;
+
+        /// <summary>
+        /// The archive file name used in this RPG.
+        /// Used for flexible name files, usually "OHRRPGCE" in newer RPG files.
+        /// </summary>
+        string archinym;
+
+        #endregion
+
+        #region .GEN Data
+
+
+
+        #endregion
+
+        private ushort CalculatePWHash(string password)
         {
-            get
+            if (password.Length == 0) return 0;
+            else
             {
-                return instance;
+                uint hash = 0;
+                foreach (char c in password)
+                {
+                    hash = (uint)(hash * 3 + c * 31);
+                }
+                return (ushort)((hash & 511) | 512);
             }
         }
 
-        public void ReadFromFile(string fileName)
+        public enum LoadedState
         {
+            /// <summary>
+            /// No RPG file is loaded.
+            /// </summary>
+            NotLoaded, 
 
-        }
+            /// <summary>
+            /// A password exists and no operations can be done on the RPG file until it is provided.
+            /// </summary>
+            PasswordNeeded, 
 
-        public void WriteToFile(string fileName)
-        {
+            /// <summary>
+            /// The password provided did not match what was stored in the GEN file and no operations can be done on the RPG file.
+            /// </summary>
+            BadPassword,
 
+            /// <summary>
+            /// The RPG file is loaded and ready to use.
+            /// </summary>
+            OK
         }
     }
 }
